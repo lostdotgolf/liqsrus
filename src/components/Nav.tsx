430a6b160cb96bfb26b9d36035280973a687edb8
Reed Loustalot <r@gofl.io>
2022-06-06 11:27:39 -0700

    more styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,9 @@
+import logo from "../images/logo.svg";
+
 export default function Nav() {
   return (
     <nav className="nav">
-      <a href="/">logo</a>
+      <img src={logo} alt="licker" height="50px" width="50px" />
       <a href="/">liqs 'r us</a>
     </nav>
   );
